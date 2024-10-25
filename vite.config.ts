ca6017f3493a1c5810ac6ec0681ec91edf5af007
Crazystal <69176305+Cryszzz@users.noreply.github.com>
2024-10-24 22:07:17 -0400

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import rawPlugin from 'vite-raw-plugin';
+import { defineConfig } from 'vite'
 
-export default {
-  plugins: [
-    rawPlugin({
-      fileRegex: /\.wgsl$/,
-    }),
-  ],
-};
+export default defineConfig({
+    build: {
+        target: 'esnext'
+    },
+    base: process.env.GITHUB_ACTIONS_BASE || undefined,
+    plugins: [
+        rawPlugin({
+            fileRegex: /\.wgsl$/,
+        }),
+    ],
+})
